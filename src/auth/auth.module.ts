2f01e68f50c278c9366efc850ba791ff0d878f7a
hamza5645 <hamzaosama5645@outlook.com>
2025-08-24 09:00:20 +0300

    Installed Prisma, starting to configure it

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
-@Module ({
-    controllers: [AuthController],
-    providers: [AuthService],
+@Module({
+  imports: [],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-    signup() {
-        return { msg: 'I have signed up' }
-    }
-    
-    signin() {
-        return { msg: 'I have signed in' }
-    }
-}   
+  signup() {
+    return { msg: 'I have signed up' };
+  }
+
+  signin() {
+    return { msg: 'I have signed in' };
+  }
+}
